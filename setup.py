7a16cb0406ee81531c10580cacb482cc89b53b94
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-12-15 19:29:05 -0700

    setup code added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Visa_Prediction",
+    version = "0.0.1",
+    author = "Atharva Chundurwar",
+    author_email = "atharvachundurwar841@gmail.com",
+    packages = find_packages()
+)
