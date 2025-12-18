150c1dcfa6cbfc8a0eacf2fd3c7cc0b33edc4cdb
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-12-18 12:51:30 -0700

    connection to mongo db client code added

---
+++ Visa_Prediction/configuration/mongo_db_conn.py
@@ -0,0 +1,35 @@
+import sys
+
+from Visa_Prediction.exception import USvisaException
+from Visa_Prediction.logger import logging
+
+import os
+from Visa_Prediction.constants import DB_NAME, MONGO_CONNECTION_URL
+import pymongo
+import certifi
+
+ca = certifi.where()
+
+class MongoDBClient:
+    """
+    Class Name :   export_data_into_feature_store
+    Description :   This method exports the dataframe from mongodb feature store as dataframe 
+    
+    Output      :   connection to mongodb database
+    On Failure  :   raises an exception
+    """
+    client = None
+
+    def __init__(self, database_name=DB_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv(MONGO_CONNECTION_URL)
+                if mongo_db_url is None:
+                    raise Exception(f"Environment key: {MONGO_CONNECTION_URL} is not set.")
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+            logging.info("MongoDB connection succesfull")
+        except Exception as e:
+            raise USvisaException(e,sys)
