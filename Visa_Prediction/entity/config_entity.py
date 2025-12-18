a8768d743f2c30da16601f53b3d712d01b24dd18
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-12-18 12:51:07 -0700

    config and artifact entity code added for data ingestion

---
+++ Visa_Prediction/entity/config_entity.py
@@ -0,0 +1,34 @@
+"""
+We have the config entity file because we will connect our constants to our data ingestion code and the constants 
+just have variables, but this code will convert it to proper path and we will be able to use it in our data ingestion code.
+"""
+
+import os
+from pathlib import Path 
+from Visa_Prediction.constants import *
+from dataclasses import dataclass
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+""" 
+Using the dataclass decorator to create a class that will hold the configuration for our training pipeline.
+We do not need to define the __init__ method because the dataclass decorator will automatically generate it for us.
+"""
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    collection_name:str = DATA_INGESTION_COLLECTION_NAME
