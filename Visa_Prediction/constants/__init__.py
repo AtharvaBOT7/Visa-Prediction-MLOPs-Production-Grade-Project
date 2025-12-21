0eda6b05ea17c80e4e8eabe9ab9722c1b72b6dcc
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-12-21 12:25:05 -0700

    constants related to data validation added

--- Visa_Prediction/constants/__init__.py
+++ Visa_Prediction/constants/__init__.py
@@ -1,6 +1,7 @@
 import os 
 from datetime import datetime
 from dotenv import load_dotenv
+import date
 
 load_dotenv()
 
@@ -18,6 +19,11 @@ TEST_FILE_NAME: str = "test.csv"
 
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 """ 
 These are the Data Ingestion related constant vairables and we save all these in this file because if we 
 later want to change the values we can easily change it from here and it will reflect in the entire project 
@@ -29,3 +35,9 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+"""
+These are the Data Validation related constants.
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
