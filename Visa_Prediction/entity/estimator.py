92900ea0ef46bd800534022e705de15667db21a3
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-12-27 10:39:00 -0700

    VisaModel function added to estimator file

--- Visa_Prediction/entity/estimator.py
+++ Visa_Prediction/entity/estimator.py
@@ -18,3 +18,32 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self.asdict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+    
+
+class VisaModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, dataframe: DataFrame) -> DataFrame:
+        """
+        This function will accept the raw inpits and then transform that using preprocessing object, this will make sure
+        that same preprocessing steps are followed which were used during training.
+        After this it will do the prediction using trained model object.
+        """
+        try:
+            logging.info("Using trained model to get predictions")
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+
+            logging.info("Used preprocessing object to get the predictions")
+            return self.trained_model_object.predict(transformed_feature)
+
+        except Exception as e:
+            raise visaException(e, sys) from e
+        
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+    
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+
